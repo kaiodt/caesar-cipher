02f40a16b0e554cac6a04fa0d7a61f338be2edf4
Kaio Rocha <kaiodtr@gmail.com>
2025-01-24 19:12:27 -0300

    style: apply `blue` and `ruff` style fixes

--- caesar_cipher/cli.py
+++ caesar_cipher/cli.py
@@ -1,10 +1,23 @@
-from caesar import encrypt, decrypt
 from argparse import ArgumentParser
+
+from caesar import decrypt, encrypt
 
 parser = ArgumentParser(description="Caesar's Cipher")
 parser.add_argument('text', help='Text to encrypt/decrypt', type=str)
-parser.add_argument('-n', help='Shift value', default=13, type=int, required=False)
-parser.add_argument('-d', help='Decrypt text', required=False, action='store_true')
+parser.add_argument(
+    '-n',
+    help='Shift value',
+    default=13,
+    type=int,
+    required=False,
+)
+parser.add_argument(
+    '-d',
+    help='Decrypt text',
+    required=False,
+    action='store_true',
+)
+
 
 def cli():
     args = parser.parse_args()
@@ -12,8 +25,9 @@ def cli():
         result = decrypt(args.text, args.n)
     else:
         result = encrypt(args.text, args.n)
-    
+
     print(f'Input: {args.text}')
     print(f'Output: {result}')
+
 
 cli()
